14eb01705886ecda137645601510d1aedd99be42
Jenny from the Block <sunflowermaster234@yahoo.com>
2022-08-14 18:03:47 -0400

    Merging with Desktop files

---
+++ Exercise 11.py
@@ -0,0 +1,8 @@
+PEOPLE = [{"first":"Jennifer","last":"Dakkak","email":"jennifer.dakkak@gmail.com"},{"first":"Olivia","last":"Dakkak","email":"olivia.dakkak@gmail.com"},{"first":"Sylvia","last":"Dakkak","email":"sylvia.dakkak@gmail.com"}]
+
+def alphabetize_names(phone_book):
+	first_name_sorted = None
+	last_name_sorted = None
+	return first_name_sorted, last_name_sorted
+
+print(alphabetize_names(PEOPLE))
